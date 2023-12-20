5e688c778c05896cf7d4c1d280c59997af5917b4
Jack Lingle <captainjack0818@gmail.com>
2023-12-20 11:24:19 -0500

    Initial Commit

---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Jack Lingle on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
